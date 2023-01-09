51cf7720c8894a5a23db46ff16b05756461a1ae1
HEERA LAL <54330840+heerendrachoudhary@users.noreply.github.com>
2023-01-09 12:54:46 +0530

    added and AOI
    Name and areas of interest.

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Heera Lal';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Artificial Intelligence | Data Science | Machine Learning | Deep Learning';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Thank you!!!';
 
   return {
     name,
